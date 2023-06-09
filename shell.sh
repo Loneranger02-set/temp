2c9da680df77daa456b814684243ea4c02eadfe9
Loneranger02-set <anishas.cs19@rvce.edu.in>
2023-06-09 16:07:42 +0530

    Create shell.sh

---
+++ shell.sh
@@ -0,0 +1,8 @@
+echo "Info: Using node version: `node -v` and npm version: `npm -v` and yarn version: `yarn -v`"
+set -e
+SERVICE_DIR='packages/web'
+
+cd $WORKSPACE
+rm -rf .npmrc
+cd $WORKSPACE/$SERVICE_DIR
+time yarn install
