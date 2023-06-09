65721e390d0c514e5bbc9bea87ed52e2244ea539
Loneranger02-set <anishas.cs19@rvce.edu.in>
2023-06-09 15:34:00 +0530

    Update install.py

--- install.py
+++ install.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+'''#!/usr/bin/env bash
 # run build
 echo "Info: Using node version: `node -v` and npm version: `npm -v` and yarn version: `yarn -v`"
 set -e
@@ -7,4 +7,5 @@ SERVICE_DIR='packages/web'
 cd $WORKSPACE
 rm -rf .npmrc
 cd $WORKSPACE/$SERVICE_DIR
-time yarn install
+time yarn install'''
+print("Hello there")
